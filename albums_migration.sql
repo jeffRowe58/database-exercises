c6e83600ce1fd9316e11b740d2cd7af3c3ebec02
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-06-17 16:28:43 -0500

    added gitignore

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name VARCHAR (50) NOT NULL,
     album_name VARCHAR (50) NOT NULL,
-    release_date DATE NOT NULL,
-    sales_number INT UNSIGNED NOT NULL,
+    release_date INT(4) NOT NULL,
+    sales_number DECIMAL(15, 2) UNSIGNED NOT NULL,
     genre VARCHAR (30) NOT NULL,
     PRIMARY KEY (id)
 );
