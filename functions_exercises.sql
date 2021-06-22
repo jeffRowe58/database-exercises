13ba9d555df9eedcb7d02537c4dd7f90a35f86be
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-06-22 10:05:28 -0500

    aliases

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,11 +2,20 @@ USE employees;
 
 SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'E%E';
 
-SELECT CONCAT(first_name, ' ', last_name) FROM employees
-WHERE birth_date LIKE '____-12-25';
+# SELECT CONCAT(first_name, ' ', last_name) FROM employees
+# WHERE birth_date LIKE '____-12-25';
 
-SELECT CONCAT(first_name, ' ', last_name) FROM employees
-WHERE birth_date LIKE '____-12-25' AND(YEAR(hire_date) BETWEEN 1990 AND 1999);
+SELECT * FROM employees
+WHERE DAY(birth_date) = 25
+  AND MONTH(birth_date) = 12;
+
+# SELECT CONCAT(first_name, ' ', last_name) FROM employees
+# WHERE birth_date LIKE '____-12-25' AND(YEAR(hire_date) BETWEEN 1990 AND 1999);
+
+SELECT * FROM employees
+WHERE DAY(birth_date) = 25
+  AND MONTH(birth_date) = 12
+  AND YEAR(hire_date) LIKE '199%';
 
 SELECT CONCAT(first_name, ' ', last_name) FROM employees
 WHERE birth_date LIKE '____-12-25' AND
